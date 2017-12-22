69e166931a1d32e3a7adec93aa1977de28567f2b
ajiawei <3136320069@qq.com>
2017-12-22 16:53:37 +0800

    没有bug c3p0 数据库连接池  统一使用@RestController 相当于@controller+@responseBody

--- commonweal/src/main/java/cn/tuyuan/commonweal/controller/PersonController.java
+++ commonweal/src/main/java/cn/tuyuan/commonweal/controller/PersonController.java
@@ -2,22 +2,26 @@ package cn.tuyuan.commonweal.controller;
 
 
 import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import cn.tuyuan.commonweal.service.PersonService;
 
-@Controller("personController")
+@RestController("personController")
+@RequestMapping("/person")
 public class PersonController {
 
 	@Resource
 	public PersonService personService;
 	
 	@RequestMapping(value="/test",method=RequestMethod.GET)
-	public String getPerson() {
+	public ModelAndView getPerson() {
 		System.out.println("你好");
-		return "hello";
+		return new ModelAndView("hello");
 		
 	}
 }
